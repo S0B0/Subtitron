3b134bc928980d0c44dcb280a2634f6f875dd998
S0 B0 <7665237-S0B0@users.noreply.gitlab.com>
2020-11-19 15:22:33 +0200

    updated the script with basic functionalites. Discovered it now has encoding errors. Save button broke.

--- main.py
+++ main.py
@@ -1,33 +1,64 @@
 from tkinter import *
 from tkinter import filedialog
+import re
 
 root = Tk()
 root.title('SUBTITRON')
 root.geometry("500x300")
 
+#Create Main Frame
+my_frame = Frame(root)
+my_frame.pack(pady=10)
+
+#MENU
+my_menu = Menu(root)
+root.config(menu = my_menu)
+
+#File Menu
+file_menu = Menu(my_menu,tearoff=False)
+my_menu.add_cascade(label = "File",menu = file_menu)
+file_menu.add_command(label="Open")
+file_menu.add_command(label="Save as")
+file_menu.add_separator()
+file_menu.add_command(label="Exit")
+
+
+#Edit Menu
+edit_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label = "File",menu = edit_menu)
+edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy")
+edit_menu.add_command(label="Undo")
+edit_menu.add_command(label="Redo")
+
+
+def subtitles():
+    sub = open_file()
+    my_list = sub.readlines()
+    print("ORIGINAL" + str(my_list))
+
+    result = [letter.replace('ã', 'ă') for letter in my_list]
+    print("RESULT!! : " + str(result))
+    save_file(result)
 
 def open_file():
-    subtitle_file = filedialog.askopenfilename(initialdir = "C:/Users",title="Open File", filetypes=(("Subtitle Files","*.srt"),))
-    subtitle_file = open(subtitle_file,'r+')
-    my_list = subtitle_file.readlines()
-    print(my_list)
 
-    count = 0
-    for i in my_list:
-        if "ã" in i:
-            count+=1
-            my_list = subtitle_file.replace('ã','ă')
+   subtitle_file = filedialog.askopenfilename(initialdir = "C:/Users",title="Open File", filetypes=(("Subtitle Files","*.srt"),))
+   subtitle_file = open(subtitle_file, 'r')
+   return subtitle_file
 
-    print("apare de : ")
-    print(count)
-    print(my_list)
+def save_file(file):
+    # SAVING RESULT ON THE FILE
+    subtitle_file2 = filedialog.askopenfilename(initialdir="C:/Users", title="Open File", filetypes=(("Subtitle Files", "*.srt"),))
+    subtitle_file2 = open(subtitle_file2, 'w')
+    subtitle_file2.write(str(file))
 
 
 
-
-
-open_btn = Button(root, text="Open File", command = open_file)
+open_btn = Button(root, text="Open File", command = subtitles)
 open_btn.pack(pady=20)
 
+save_btn = Button(root, text="Save File", command = save_file)
+save_btn.pack(pady=40)
 
 root.mainloop()
